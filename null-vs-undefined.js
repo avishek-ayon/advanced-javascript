7737128eb94e18fc421552a9002264b2086aa541
avishek-ayon <avishek.ayon96@gmail.com>
2020-03-04 22:24:13 +0600

    done

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+//null-vs-undefined.js
+// let pakhi;
+// console.log(pakhi);
+
+// function add(x1,x2)
+// {
+//     console.log(x1+x2);
+//     return (x1+x2);
+// }
+// const result=add(5,7);
+// console.log(result);
+
+// function add(x1,x2)
+// {
+//     console.log(x2);
+// }
+
+// const result=add(7,13);
+// console.log(result);
+
+const premik={name:"avi" ,phone:01861};
+console.log(premik.gf);
+
+const array=[2,2,5];
+console.log(array[3]);
---
+++ truthy-falshy.js
@@ -0,0 +1,47 @@
+/**
+ false
+ 0
+ ""
+ undefined
+ NaN
+ null
+ */
+/**
+ truthy
+ "0"
+ {}
+ []
+ ' '
+ " "
+ */
+
+// const age=0;
+
+// if(age==0)
+// {
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+
+
+
+//const name;
+//console.log(name);
+// let name;
+//let name=undefined;
+// let name=null;
+//let name=false;
+const name={};
+//let name=[];
+
+
+if(name)
+{
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
