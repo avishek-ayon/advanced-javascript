1f26e9443cf8c156cfab13c9fdd0fff52c7cedeb
avishek-ayon <avishek.ayon96@gmail.com>
2020-03-04 23:09:51 +0600

    done

---
+++ double-triple-equal.js
@@ -0,0 +1,45 @@
+//(==) type check kore na, sudu value check kore
+//(===) type check kore and value check kore  
+
+//const first=2;
+// const last=2;
+// const last="2";
+/**const first=1;
+const last=true;*/
+/**const first=0;
+const last=false;*/
+
+// if(first==last)
+// {
+//     console.log("condition true");
+// }
+// else{
+//     console.log("condition false");
+// }
+
+
+/**const first=1;
+// const last=true;*/
+// const first=2;
+// //const last="2";
+// const last=2;
+
+
+// if(first===last)
+// {
+//     console.log("condition true");
+// }
+// else{
+//     console.log("condition false");
+// }
+
+
+
+
+if ("2"===2){
+    console.log("Inside if");
+  }
+  
+  else{
+    console.log("Inside else");
+  }
