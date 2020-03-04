cc93a9b2694f4b125b1ce02da99fa9278ee0419d
avishek-ayon <avishek.ayon96@gmail.com>
2020-03-05 00:29:42 +0600

    done

---
+++ map-filter.js
@@ -0,0 +1,60 @@
+const numbers=[3,4,5,6,7,8];
+const output=[];
+
+// for(let i=0;i<numbers.length;i++)
+// {
+//     const element=numbers[i];//why not let
+//     const result=element*element;
+//     output.push(result);
+// }
+
+// console.log(output);
+
+
+// function Square(element)
+// {
+//     console.log(element*element);
+// }
+
+//or
+//const Square=element=>element*element;
+
+
+//map
+
+// numbers.map(function(element,index,array)
+// {
+//     console.log(element,index,array);
+// })
+
+
+// const result=numbers.map(function(element)
+// {
+//     return element * element;
+
+// })
+
+//or
+//arrow diye likha jai
+
+// const result=numbers.map(element=>element * element);
+
+// console.log(result);
+
+
+ // //Filter
+
+// const result=numbers.filter(x=>x>5)
+// {
+//     console.log(result);
+    
+// }
+
+
+//Find
+
+const result=numbers.find(x=>x>5)
+{
+    console.log(result);
+    
+}
