228740c62e7d4f82bb79364c38b48ac9ee338d4e
avishek-ayon <avishek.ayon96@gmail.com>
2020-03-05 02:51:46 +0600

    done

---
+++ array-slice.js
@@ -0,0 +1,17 @@
+// const nums=[1,5,7,8,9,11,15];
+// const part=nums.slice(2,4);
+// console.log(part);
+// console.log(nums);
+
+
+// const nums=[1,5,7,8,9,11,15];
+// const part=nums.splice(2,4);
+// console.log(part);
+// console.log(nums);
+
+const nums=[1,5,7,8,9,11,15];
+//const part=nums.join("");
+//const part=nums.join(",");
+const part=nums.join(".");
+console.log(part);
+console.log(nums);
